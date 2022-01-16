6fca7d7ffa218b16c9586bf2311fe0515060ecfb
Rajat <rajat_agarwal@persistent.com>
2022-01-16 23:50:27 +0530

    Added fu headers

--- math.py
+++ math.py
@@ -1,22 +1,22 @@
- # Add implemetation
+
 def add(x,y):
     return x + y
 
-# subtract implemetation
+
 def substract(x,y)
     return x-y                  # on master branch 
 
-# multiple implementation
+
 def multiple(x,y)
     return x*y              # on Bug456 branch
 
-# divide implemetation
+
 def divide(x,y)                 # on master branch 
     if y==0:
         return DIVIDE_BY_0_ERROE
     else :
         return x/y
 
-# square implementation
+
 def square(x):
     return x*x
